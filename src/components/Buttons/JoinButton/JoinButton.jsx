d2709a8b32663e8323fca6342d8987e45d7c6e50
SongtingY <yang.song@northeastern.edu>
2024-03-26 11:38:06 -0700

    join-us-button

--- src/components/Buttons/JoinButton/JoinButton.jsx
+++ src/components/Buttons/JoinButton/JoinButton.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import "./styles.css";
 
 const JoinButton = ({children}) => {
+  const redirectToGoogleForm = () => {
+    window.location.href = 'https://forms.office.com/pages/responsepage.aspx?id=gcLuqKOqrk2sm5o5i5IV5y2eU7UxhlxLtYNLUzoz85RUOEpZU04yQjBDTE5ENFJQWFVIWlE3WUY3VS4u';
+  };
+
   return (
-    <button className="join-button">
+    <button className="join-button" onClick={redirectToGoogleForm}>
         {children}
   </button>
   );
