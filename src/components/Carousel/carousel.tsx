1cb0f3e33466fe15e72f27d80b767f78cbf2ef6b
avmanoj14 <amara.manoj14@gmail.com>
2024-04-08 21:23:48 -0700

    carousel-component

---
+++ src/components/Carousel/carousel.tsx
@@ -0,0 +1,73 @@
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './styles.css'
+
+import slide_image_1 from "../../assets/carousel/silder1.jpg";
+import slide_image_2 from "../../assets/carousel/slider2.jpg";
+import slide_image_3 from "../../assets/carousel/slider3.jpg";
+
+
+function App() {
+  return (
+    <div className="container">
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+        <SwiperSlide>
+          <img src={slide_image_1} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_2} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_3} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_1} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_2} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_3} alt="slide_image" />
+        </SwiperSlide>
+        
+
+        <div className="slider-controler">
+          {/* <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div> */}
+          {/* <div className="swiper-pagination"></div> */}
+        </div>
+      </Swiper>
+    </div>
+  );
+}
+
+export default App;
